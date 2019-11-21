2e8c334713f96a581f27e4e4eaead8aa44e21e3e
Joris van der Straten <i885981@fhict.nl>
2019-11-21 14:47:04 +0100

    Minor changes: - Added paramater for downloading specific datasets - Added joinThemAll table

--- R/Functions.R
+++ R/Functions.R
@@ -1,19 +1,33 @@
 id <- "6ccbb5c4-fa22-45dc-ad57-74c29cc8b3d6"
 
 #' @importFrom magrittr %>%
-loadTable <- function(tableName) {
+loadTable <- function(id, tableName) {
   jsonlite::fromJSON(paste0("http://i885981core.venus.fhict.nl/table/", tableName, "/", id)) %>% 
     dplyr::mutate_at(dplyr::vars(dplyr::ends_with("date", ignore.case = TRUE)), lubridate::as_datetime)
 }
 
 
 #' loads all tables
+#' @importFrom magrittr %>%
 #' @export
-loadAllTables <- function()
+loadAllTables <- function(id = NULL)
 {
+  if (is.null(id)) {id <- "6ccbb5c4-fa22-45dc-ad57-74c29cc8b3d6"}
+  con <- DBI::dbConnect(RSQLite::SQLite(), ":memory:")
+  
   tables <- c("Customers", "Employees", "Orders", "OrderLines", "Products", "ProductCategories", "ProductPrices", "SalesRegions", "Stock")
   
   for (i in 1:length(tables)){
-    assign(tolower(tables[i]), loadTable(tables[i]), envir = parent.frame())
+    assign(tolower(tables[i]), loadTable(id, tables[i]), envir = parent.frame())
+    dplyr::copy_to(con, get(tolower(tables[i])), tolower(tables[i]))
   }
+  
+  # JoinThemAll
+  assign("vwJoinAll", jsonlite::fromJSON(paste0("http://i885981core.venus.fhict.nl/json/", id)) %>% 
+        dplyr::mutate_at(dplyr::vars(dplyr::ends_with("date")), lubridate::as_datetime),
+        envir = parent.frame())
+  
+  dplyr::copy_to(con, get("vwJoinAll"), "vwJoinAll")
+  
+  con
 }
